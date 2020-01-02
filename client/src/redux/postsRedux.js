0d80e6949a1e22068b2f3da1a82adcf5a4df6f6c
fabianzielinski <fabz@o2.pl>
2020-01-02 21:44:04 +0100

    Add Redux-thunk

---
+++ client/src/redux/postsRedux.js
@@ -0,0 +1,43 @@
+/* SELECTORS */
+
+export const getPosts = ({ posts }) => posts;
+
+// action name creator
+const reducerName = 'posts';
+const createActionName = name => `app/${reducerName}/${name}`;
+
+/* ACTIONS */
+
+export const LOAD_POSTS = createActionName('LOAD_POSTS');
+
+export const loadPosts = payload => ({ payload, type: LOAD_POSTS });
+
+/* INITIAL STATE */
+
+const initialState = [];
+
+/* REDUCER */
+
+export default function reducer(statePart = initialState, action = {}) {
+  switch (action.type) {
+    case LOAD_POSTS:
+      return [ ...action.payload ];
+    default:
+      return statePart;
+  }
+};
+
+/* THUNKS */
+
+export const loadPostsRequest = () => {
+  return dispatch => {
+
+    console.log('Request started...');
+    setTimeout(() => {
+      const arr = [{ id: 'a3fssdc1', title: 'Test', content: 'Lorem Ipsum' }];
+      dispatch(loadPosts(arr));
+      console.log('Request finished after 2sec!');
+    }, 2000);
+
+  };
+};
